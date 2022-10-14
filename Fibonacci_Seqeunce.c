b9db0f88157b98a9b943c95e0cac98e400fde499
wjy4ng <aeillou9@naver.com>
2022-10-14 14:11:58 +0900

    Fibonacci Sequence Code File Update

--- Fibonacci_Seqeunce.c
+++ Fibonacci_Seqeunce.c
@@ -13,21 +13,22 @@
 
 #include <stdio.h>
 
+// 피보나치 함수 프로토타입 선언
 int fib(int n);
 
 int main(){
 	int num;
 
 	printf("input = ");
-	scanf("%d", &num);
+	scanf("%d", &num); // 반복문을 실행하기 위해 입력값 num을 받는다
 
-	for(int i=0; i<=num; i++){
-		printf("%d ", fib(i));
+	for(int i=0; i<=num; i++){ // num 만큼 반복
+		printf("%d ", fib(i)); // 0~num 까지 피보나치 함수에 대입
 	}
 }
 
 int fib(int n){
-	if(n==0) return 0;
-	if(n==1) return 1;
-	return fib(n-1) + fib(n-2);
+	if(n==0) return 0; // n이 0이면 0을 반환하여 출력
+	if(n==1) return 1; // n이 1이면 1을 반환하여 출력
+	return fib(n-1) + fib(n-2); // n부터 스택에 쌓이고 이어 n-1, n-2도 스택에 계속해서 쌓임
 }
