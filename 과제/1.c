ea75fd78278182b2093fad4a694043ae29e1a405
wjy4ng <aeillou9@naver.com>
2022-12-06 23:38:52 +0900

    2-2 과제2 업로드

---
+++ 과제/1.c
@@ -0,0 +1,156 @@
+/*
+	* 학번 및 이름 : 202101354 정보통신공학과 양원준
+	* 목적 및 개요 : 2-2학기 자료구조및알고리즘 과제 <교재 연결리스트I 연습문제 변형>
+	* 과제수행날짜 : 2022.12.05(월)
+	* 사용언어 : C언어
+	* 프로그램 작성환경 및 실행환경 :
+	*	OS : Kali GNU/Linux Rolling 2022.3
+	*	IDE : vim editor 9.0
+	*	Compiler gcc 11.3.0
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_ArraySize 100
+
+// 노드 구조체
+typedef int element;
+typedef struct ListNode{
+	element data;
+	struct ListNode *link;
+} ListNode;
+
+// 노드 ADT 구현
+ListNode* insert_first(ListNode *head, element value);
+ListNode* insert(ListNode *head, ListNode *pre, element value);
+ListNode* delete_first(ListNode *head);
+ListNode* delete(ListNode *head, ListNode *pre);
+void print_list(ListNode *head);
+element get_entry(ListNode *head, int index);
+int get_length(ListNode *head);
+ListNode* reverse(ListNode *head);
+void split(ListNode *head);
+
+
+// 메인 함수
+int main(){
+	ListNode *A_head = NULL;
+	int size=0, arr[MAX_ArraySize];
+
+	printf("입력 값 크기 : ");
+	scanf("%d", &size);
+
+	printf("정수를 입력하시오: ");
+	for(int i=0; i<size; i++) scanf("%d", &arr[i]); // 정수 입력 받기
+	for(int i=size-1; i>=0; i--) A_head = insert_first(A_head, arr[i]); // A 리스트에 넣기
+
+	printf("\nsplit 함수 사용 전 A : ");
+	print_list(A_head); // A 리스트 출력
+	
+	split(A_head); // 분리 함수 호출
+
+	printf("split 함수 사용 후 A : ");
+	print_list(A_head); // split 함수가 A를 변경시키지 않음
+
+	return 0;
+}
+
+void split(ListNode *head){
+	ListNode *B_head = NULL; // B 헤드 포인터 생성
+	ListNode *C_head = NULL; // C 헤드 포인터 생성
+	ListNode *D_head = NULL; // D 헤드 포인터 생성
+	ListNode *p = (ListNode *)malloc(sizeof(ListNode)); // 단순 연결 리스트 객체 생성
+	int count=1; // 3으로 나눌 값
+
+	p = head; // split 함수가 A를 변경하지 않도록 p 생성 후 A를 복사
+	while(p != NULL){
+		if(count % 3 == 1) B_head = insert_first(B_head, p->data); // 1, 4, 7번째
+		else if(count % 3 == 2) C_head = insert_first(C_head, p->data); // 2, 5, 8번째
+		else D_head = insert_first(D_head, p->data); // 3, 6, 9번째
+
+		count++;
+		p = p->link;
+	}
+
+	printf("B: "); print_list(reverse(B_head)); // 리스트는 역순을 취해주어야 알맞은 순서로 나옴
+	printf("C: "); print_list(reverse(C_head));
+	printf("D: "); print_list(reverse(D_head));
+}
+
+ListNode* insert_first(ListNode *head, element value){
+	ListNode *p = (ListNode *)malloc(sizeof(ListNode));
+	p->data = value;
+	p->link = head;
+	head = p;
+	return head;
+}
+
+ListNode* insert(ListNode *head, ListNode *pre, element value){
+	ListNode *p = (ListNode *)malloc(sizeof(ListNode));
+	p->data = value;
+	p->link = pre->link;
+	pre->link = p;
+	return head;
+}
+
+ListNode* delete_first(ListNode *head){
+	ListNode *removed;
+	if(head == NULL) return NULL;
+	removed = head;
+	head = removed->link;
+	free(removed);
+	return head;
+}
+
+ListNode* delete(ListNode *head, ListNode *pre){
+	ListNode *removed;
+	removed = pre->link;
+	pre->link = removed->link;
+	free(removed);
+	return head;
+}
+
+void print_list(ListNode *head){
+	for(ListNode *p=head; p!=NULL; p=p->link){
+		printf("%d->", p->data);
+	}
+	printf("NULL\n");
+}
+
+element get_entry(ListNode *head, int index){
+	ListNode *p;
+
+	p = head;
+	for(int i=0; i<index; i++, p=p->link){
+		if(p==NULL) break;
+	}
+	return p->data;
+}
+
+int get_length(ListNode *head){
+	ListNode *p;
+	int count=0;
+
+	p = head;
+	while(p != NULL){
+		count++;
+		p = p->link;
+	}
+	return count;
+}
+
+ListNode* reverse(ListNode *head){
+	ListNode *p, *q, *r;
+
+	p = head;
+	q = NULL;
+	while(p != NULL){
+		r = q;
+		q = p;
+		p = p->link;
+		q->link = r;
+	}
+
+	return q;
+}
+
---
+++ 과제/2.c
@@ -0,0 +1,104 @@
+/*
+	* 학번 및 이름 : 202101354 정보통신공학과 양원준
+	* 목적 및 개요 : 2-2학기 자료구조및알고리즘 과제 <교재 연결리스트II 연습문제>
+	* 과제수행날짜 : 2022.12.06(화)
+	* 사용언어 : C언어
+	* 프로그램 작성환경 및 실행환경 :
+	*	OS : Kali GNU/Linux Rolling 2022.3
+	*	IDE : vim editor 9.0
+	*	Compiler gcc 11.3.0
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX_ArraySize 100
+
+// 원형 연결 리스트 구조체
+typedef int element;
+typedef struct ListNode{
+	element data;
+	struct ListNode *link;
+} ListNode;
+
+// 노드 ADT 구현
+ListNode* insert_first(ListNode *head, element value){
+	ListNode *p = (ListNode *)malloc(sizeof(ListNode));
+	p->data = value;
+
+	if(head == NULL){
+		head = p;
+		p->link = head;
+	}
+	else{
+		p->link = head->link;
+		head->link = p;
+	}
+	return head;
+}
+
+ListNode* insert_last(ListNode* head, element value){
+	ListNode *p = (ListNode *)malloc(sizeof(ListNode));
+	p->data = value;
+
+	if(head == NULL){
+		head = p;
+		p->link = head;
+	}
+	else{
+		p->link = head->link;
+		head->link = p;
+		head = p;
+	}
+	return head;
+}
+
+void print_list(ListNode *head){
+	ListNode *p;
+
+	if(head == NULL) return;
+	p = head->link;
+	do{
+		printf("%d->", p->data);
+		p = p->link;
+	} while(p!=head);
+	printf("%d->\n", p->data); // 헤드 포인터 출력
+}
+
+// 탐색 함수
+ListNode* search(ListNode *head, element value){
+	ListNode *p;
+
+	if(head==NULL) return 0;
+	p = head;
+	do{
+		if(p->data == value) return p;
+		p = p->link;
+	} while(p!=head);
+	return 0;
+}
+
+// 메인 함수
+int main(){
+	ListNode *head = NULL;
+	int size=0, arr[MAX_ArraySize], value=0;
+	int* result;
+
+	printf("입력 값 크기 : ");
+	scanf("%d", &size);
+
+	printf("정수를 입력하시오: ");
+	for(int i=0; i<size; i++) scanf("%d", &arr[i]); // 정수 입력 받기
+	for(int i=0; i<size; i++) head = insert_last(head, arr[i]); // 리스트에 넣기
+
+	printf("리스트 : "); print_list(head);
+
+	printf("탐색하려는 값 : ");
+	scanf("%d", &value);
+
+	result = (int *)search(head, value);
+	if(result != 0) printf("%d이/가 리스트에 존재합니다.\n", value);
+	else printf("%d이/가 리스트에 존재하지 않습니다.\n", value);
+	return 0;
+}
+
