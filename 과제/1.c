a0fe3cc10031087418c79d0aabb1361c67eeaf00
wjy4ng <aeillou9@naver.com>
2022-12-08 02:04:27 +0900

    과제 업로드

--- 과제/1.c
+++ 과제/1.c
@@ -13,63 +13,53 @@
 #include <stdlib.h>
 #define MAX_ArraySize 100
 
-// 노드 구조체
 typedef int element;
 typedef struct ListNode{
 	element data;
 	struct ListNode *link;
 } ListNode;
 
-// 노드 ADT 구현
 ListNode* insert_first(ListNode *head, element value);
 void print_list(ListNode *head);
-ListNode* reverse(ListNode *head);
-void split(ListNode *head);
+void split(ListNode *head, int size);
 
 
-// 메인 함수
 int main(){
 	ListNode *A_head = NULL;
 	int size=0, arr[MAX_ArraySize];
 
-	printf("입력 값 크기 : ");
+	printf("입력 값 크기 : "); // 만약 5를 넣으면
 	scanf("%d", &size);
 
-	printf("정수를 입력하시오: ");
-	for(int i=0; i<size; i++) scanf("%d", &arr[i]); // 정수 입력 받기
-	for(int i=size-1; i>=0; i--) A_head = insert_first(A_head, arr[i]); // A 리스트에 넣기
+	printf("정수를 입력하시오 : ");
+	for(int i=0; i<size; i++) scanf("%d", &arr[i]); // 만약 1, 2, 3, 4, 5를 넣으면
+	for(int i=0; i<size; i++) A_head = insert_first(A_head, arr[i]);
 
-	printf("\nsplit 함수 사용 전 A : ");
-	print_list(A_head); // A 리스트 출력
-	
-	split(A_head); // 분리 함수 호출
-
-	printf("split 함수 사용 후 A : ");
-	print_list(A_head); // split 함수가 A를 변경시키지 않음
+	split(A_head, size); // 이제 A에서 B,C,D를 분리해야 함
 
 	return 0;
 }
 
-void split(ListNode *head){
+void split(ListNode *head, int size){
 	ListNode *B_head = NULL; // B 헤드 포인터 생성
 	ListNode *C_head = NULL; // C 헤드 포인터 생성
 	ListNode *D_head = NULL; // D 헤드 포인터 생성
 	ListNode *p = (ListNode *)malloc(sizeof(ListNode)); // 단순 연결 리스트 객체 생성
-	int count=1; // 3으로 나눌 값
 
 	p = head; // split 함수가 A를 변경하지 않도록 p 생성 후 A를 복사
 	while(p != NULL){
+		int count = size % 3; // 총 5번 입력했으니 5%3=2, 처음값은 나머지가 2인 C에 넣고, 다음값은 size-1이니 4%3=1이니 1에 넣고... 반복
 		if(count % 3 == 1) B_head = insert_first(B_head, p->data); // 1, 4, 7번째
 		else if(count % 3 == 2) C_head = insert_first(C_head, p->data); // 2, 5, 8번째
 		else D_head = insert_first(D_head, p->data); // 3, 6, 9번째
 
-		count++;
+		size--;
 		p = p->link;
 	}
 
-	printf("B: "); print_list(reverse(B_head)); // 리스트는 역순을 취해주어야 알맞은 순서로 나옴
-	printf("C: "); print_list(reverse(C_head));
-	printf("D: "); print_list(reverse(D_head));
+	printf("B: "); print_list(B_head);
+	printf("C: "); print_list(C_head);
+	printf("D: "); print_list(D_head);
 }
 
 ListNode* insert_first(ListNode *head, element value){
@@ -85,20 +75,5 @@ void print_list(ListNode *head){
 		printf("%d->", p->data);
 	}
 	printf("NULL\n");
-}
-
-ListNode* reverse(ListNode *head){
-	ListNode *p, *q, *r;
-
-	p = head;
-	q = NULL;
-	while(p != NULL){
-		r = q;
-		q = p;
-		p = p->link;
-		q->link = r;
-	}
-
-	return q;
 }
 
