34b4b2f6ca9d6d290181a853ded793d0e8a00d8b
wjy4ng <aeillou9@naver.com>
2022-12-07 21:59:13 +0900

    과제2-1 수정

--- 과제/1.c
+++ 과제/1.c
@@ -22,12 +22,7 @@ typedef struct ListNode{
 
 // 노드 ADT 구현
 ListNode* insert_first(ListNode *head, element value);
-ListNode* insert(ListNode *head, ListNode *pre, element value);
-ListNode* delete_first(ListNode *head);
-ListNode* delete(ListNode *head, ListNode *pre);
 void print_list(ListNode *head);
-element get_entry(ListNode *head, int index);
-int get_length(ListNode *head);
 ListNode* reverse(ListNode *head);
 void split(ListNode *head);
 
@@ -85,58 +80,11 @@ ListNode* insert_first(ListNode *head, element value){
 	return head;
 }
 
-ListNode* insert(ListNode *head, ListNode *pre, element value){
-	ListNode *p = (ListNode *)malloc(sizeof(ListNode));
-	p->data = value;
-	p->link = pre->link;
-	pre->link = p;
-	return head;
-}
-
-ListNode* delete_first(ListNode *head){
-	ListNode *removed;
-	if(head == NULL) return NULL;
-	removed = head;
-	head = removed->link;
-	free(removed);
-	return head;
-}
-
-ListNode* delete(ListNode *head, ListNode *pre){
-	ListNode *removed;
-	removed = pre->link;
-	pre->link = removed->link;
-	free(removed);
-	return head;
-}
-
 void print_list(ListNode *head){
 	for(ListNode *p=head; p!=NULL; p=p->link){
 		printf("%d->", p->data);
 	}
 	printf("NULL\n");
-}
-
-element get_entry(ListNode *head, int index){
-	ListNode *p;
-
-	p = head;
-	for(int i=0; i<index; i++, p=p->link){
-		if(p==NULL) break;
-	}
-	return p->data;
-}
-
-int get_length(ListNode *head){
-	ListNode *p;
-	int count=0;
-
-	p = head;
-	while(p != NULL){
-		count++;
-		p = p->link;
-	}
-	return count;
 }
 
 ListNode* reverse(ListNode *head){
